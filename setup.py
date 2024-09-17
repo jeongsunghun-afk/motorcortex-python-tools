d36d9beb6c035cd136454d7960bc2999f441e82c
Philippe Piatkiewitz <philippe@vectioneer.com>
2024-09-17 10:29:18 +0200

    Updated Tag

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='motorcortex-python-tools',
-      version='1.4.1',
+      version='1.4.2',
       description='Python tools for Motorcortex Engine',
       author='Philippe Piatkiewitz',
       author_email='philippe.piatkiewitz@vectioneer.com',
